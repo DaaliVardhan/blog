10b317f8cb0651f442c388ccaeb6c2c7eff39fca
Daali <daalivardhan55@gmail.com>
2022-11-23 01:11:13 +0530

    deploy changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path=require("path")
 const User=require("./models/user")
 const mongoose=require('mongoose')
 
-mongoose.connect("mongodb://localhost/test")
+mongoose.connect("mongodb+srv://daalivardhan:daali.228005@cluster0.v12h2.mongodb.net/?retryWrites=true&w=majority")
 const app=express()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -12,7 +12,11 @@ app.set("view engine","ejs")
 app.use(express.static('public'))
 
 
-
+const logger=(req,res,next)=>{
+    console.log(req.method,req.url)
+    next()
+}
+app.use(logger)
 
 app.get("/blog-list(.html)?",(req,res)=>{
     return res.sendFile(path.join(__dirname,'views','website','blog-list.html'))
